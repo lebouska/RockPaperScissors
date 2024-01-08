430da3dbad5c9fdfc22980ff2ef5b48fbadacfd7
Lebouska <149729166+lebouska@users.noreply.github.com>
2024-01-08 19:14:39 -0300

    add ui and new code

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerPoints = 0;
+let playerPoints = 0;
+
 function getComputerChoice() {
     let computer = Math.floor(Math.random() * 3 + 1);
     return computer;
@@ -11,99 +14,175 @@ function computerChoice() {
         return "paper";
     } else if (computerchoice == 3) {
         return "scissors";
-    } else {
-        return "deu merda";
     }
 }
 
-function playerChoice() {
-    Choice = prompt("Rock paper scissors go!");
-    Choice = Choice.toLowerCase();
-    return Choice;
+function tie (pChoice) {
+    textwindow.textContent= `Oh god that is going to take a while!\nWe both chose ${pChoice}.\n
+    Let's go again!\n
+    Rock, paper, scissors, shoot!`;
+}
+
+function lost(pChoice, cChoice) {
+    computerPoints += 1;
+    pointsC.textContent= `${computerPoints} points`
+    if (computerPoints == 5) {
+        textwindow.textContent = `You lost you little bitch!\nMy ${cChoice} beats you shitty ${pChoice}!`;
+        removeAllButtons();
+        let playerPoints = 0;
+        let computerPoints = 0;
+        again();
+    } else {
+        textwindow.textContent = `My ${cChoice} beats you shitty ${pChoice}! \n\n\nRock, paper, scissors, shoot!`;
+    }
+}
+
+function won(pChoice, cChoice) {
+    playerPoints += 1;
+    pointsP.textContent= `${playerPoints} points`
+    if (playerPoints == 5) {
+        textwindow.textContent = `You cheater you won!\nYour ${pChoice} beats somehow my ${cChoice}!`;
+        removeAllButtons();
+        let playerPoints = 0;
+        let computerPoints = 0;
+        again();
+    } else {
+        textwindow.textContent = `Your ${pChoice} broke my ${cChoice}! \n\n\nRock, paper, scissors, shoot!`;
+    }
+
+}
+
+function again() {
+    const playAgain = document.createElement("button");
+    playAgain.textContent = "I wan't to play again!";
+    buttons.appendChild(playAgain);
+    playAgain.addEventListener('click', () => {
+        start();
+    })
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            return "tie";
+            tie("rock");
         } else if (computerSelection == "paper") {
-            return "lost";
+            lost("rock", "paper");
         } else {
-            return "won";
+            won("rock", "scissor");
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            return "won";
+            won("paper", "rock");
         } else if (computerSelection == "paper") {
-            return "tie";
+            tie("paper");
         } else {
-            return "lost";
+            lost("rock", "scissor");
         }
-    } else if (computerSelection == "scissors") {
+    } else if (playerSelection == "scissor") {
         if (computerSelection == "rock") {
-            return "lost";
+            lost("scissor", "rock");
         } else if (computerSelection == "paper") {
-            return "won";
+            won("scissor", "paper");
         } else {
-            return "tie";
-        }
-    } else {
-        return "pass";
-    }
-}
-
-function game() {
-    console.log("Let's play rock, paper scissors!");
-    console.log("The first to win 5 games wins!");
-    let playerWins = 0
-    let computerWins = 0
-    let round = 1
-    let chances = 1
-    while (playerWins < 5 && computerWins < 5) {
-        console.log(`starting round ${round}!`)
-        let playerSelection = playerChoice();
-        let computerSelection = computerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result === "tie") {
-            console.log (`That was a tie! We both played ${playerSelection}!`);
-            round = round + 1;
-        } else if (result == "won") {
-            console.log (`You won this round! Your ${playerSelection} beat my ${computerSelection}!`);
-            playerWins = playerWins + 1;
-            round = round + 1;
-            if (playerWins !== 5) {
-                console.log (`The score now is: You won ${playerWins} and I won ${computerWins}.`);
-            } else {
-                console.log ("You won the game!");
-            }
-
-        } else if (result === "lost") {
-            console.log (`You lost this round! my ${computerSelection} beat your ${playerSelection}!`);
-            computerWins = computerWins + 1;
-            round = round + 1;
-            if (computerWins !== 5) {
-                console.log (`The score now is: You won ${playerWins} and I won ${computerWins}.`);
-            } else {
-                console.log ("I won the game!");
-            }
-        } else if (result === "pass") {
-            if (chances == 1) {
-                console.log ("I'm going to give you another chance, you can only choose between rock, paper or scissors.");
-                console.log ("If you do that again I'm going to get a free point.");
-                chances = chances - 1
-                round = round + 1;
-            } else {
-                computerWins = computerWins +1;
-                console.log ("I told you! A point for me because you didn't choose between rock, paper or scissors.");
-                if (computerWins !== 5) {
-                    console.log (`The score now is: You won ${playerWins} and I won ${computerWins}.`);
-                } else {
-                    console.log ("I won the game!");
-                }
-            }
-
+            tie("scissor");
         }
     }
 }
 
-game()
+function removeAllButtons() {
+    while (buttons.firstChild) {
+        buttons.removeChild(buttons.firstChild);
+    }
+}
+
+const textwindow = document.querySelector(".textwindow")
+const buttons = document.querySelector('.gamebuttons')
+const pointsP = document.querySelector('.pointsP')
+const pointsC = document.querySelector('.pointsC')
+
+function gameIntroduction() {
+    textwindow.textContent="It's going to be best of 5 !\n\n\nAre you ready?";
+    const yes = document.createElement("button");
+    yes.classList.add('Yes');
+    yes.textContent = "Yes"
+    buttons.appendChild(yes);
+    const btnno = document.createElement("button");
+    btnno.classList.add('No');
+    btnno.textContent = "No"
+    buttons.appendChild(yes);
+    buttons.appendChild(btnno);
+    btnno.addEventListener('click', () => {
+        bitch();
+    })
+    yes.addEventListener('click', () => {
+        textwindow.textContent="Rock, paper, scissors, shoot!";
+        removeAllButtons()
+        const rock = document.createElement('button');
+        rock.textContent = "Rock";
+        rock.classList.add('rock');
+        buttons.appendChild(rock);
+        const paper = document.createElement('button');
+        paper.textContent = "Paper";
+        paper.classList.add('paper');
+        buttons.appendChild(paper);
+        const scissor = document.createElement('button');
+        scissor.textContent = "Scissor";
+        scissor.classList.add('scissor');
+        buttons.appendChild(scissor);
+        let computerPoints = 0;
+        let playerPoints = 0;
+        rock.addEventListener('click', () => {
+            let computerCh = computerChoice();
+            playRound("rock", computerCh);
+        })
+        paper.addEventListener('click', () => {
+            let computerCh = computerChoice();
+            playRound("paper", computerCh);
+        })
+        scissor.addEventListener('click', () => {
+            let computerCh = computerChoice();
+            playRound("scissor", computerCh);
+        })
+    })
+}
+
+function bitch() {
+    textwindow.textContent="Thats why I called you A BITCH!";
+    removeAllButtons();
+    const btnChange = document.createElement('button');
+    btnChange.textContent = 'Ok, I changed my mind';
+    buttons.appendChild(btnChange);
+    btnChange.addEventListener('click', () => {
+        removeAllButtons();
+        gameIntroduction();
+    })
+}
+
+function start () {
+    computerPoints = 0;
+    playerPoints = 0;
+    pointsP.textContent= `${playerPoints} points`
+    pointsC.textContent= `${computerPoints} points`
+    removeAllButtons();
+    textwindow.textContent="Hey you, unlucky bitch!\n\n\nWant to play a game of Rock Paper Scissors?";
+    const btnyes = document.createElement('button');
+    const btnno = document.createElement('button');
+    btnyes.classList.add('yes');
+    btnno.classList.add('no');
+    btnyes.textContent = 'Yes';
+    btnno.textContent = 'No';
+
+    buttons.appendChild(btnyes);
+    buttons.appendChild(btnno);
+
+    btnno.addEventListener('click', () => {
+        bitch();
+    })
+
+    btnyes.addEventListener('click', () => {
+        removeAllButtons();
+        gameIntroduction();
+    })
+}
+
+start();
